8bf6cd7f4c85f92b18d635bf6bf7adbfac16d036
Et-17 <leland.kilborn@hotmail.com>
2025-07-13 17:21:31 -0400

    Implement basic error error handling
    I added the module `error_handling` which allows
    you to create custom error types that store the
    location of the error.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fmt::Display;
+
+use crate::error_handling::{Error, ErrorType, Location};
+
+mod error_handling;
+
+#[derive(Debug)]
+pub enum TestErrorType {
+    TestError,
+}
+
+impl ErrorType for TestErrorType {}
+
+impl PartialEq for TestErrorType {
+    fn eq(&self, _other: &Self) -> bool {
+        true
+    }
+}
+
+impl Display for TestErrorType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TestErrorType::TestError => write!(f, "This is a test error"),
+        }
+    }
+}
+
 fn main() {
+    let test_error = Error {
+        location: Location { line: 5 },
+        error: TestErrorType::TestError,
+    };
     println!("Hello, world!");
+    eprintln!("{}", test_error);
 }
